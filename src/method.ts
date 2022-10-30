c4ae6da60fc7523e910092fb4d97c9f87873eb1b
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2022-10-30 15:01:56 +0100

    adds methods object

---
+++ src/method.ts
@@ -0,0 +1,15 @@
+export default {
+  auth: 'auth.getToken',
+  user: {
+    getInfo: 'user.getInfo',
+    loved_tracks: 'user.getLovedTracks',
+    recent_tracks: 'user.getRecentTracks',
+    top_albums: 'user.getTopAlbums',
+    top_artists: 'user.getTopArtists',
+    top_tracks: 'user.getTopTracks',
+    weekly_album_chart: 'user.getWeeklyAlbumChart',
+    weekly_artist_chart: 'user.getWeeklyArtistChart',
+    weekly_chart_list: 'user.getWeeklyChartList',
+    weekly_track_chart: 'user.getWeeklyTrackChart',
+  },
+};
