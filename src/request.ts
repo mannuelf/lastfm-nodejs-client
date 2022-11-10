140f6333f5d26af9322ebb1cb6f56b4b8650df84
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2022-11-10 22:58:57 +0100

    adds params

--- src/request.ts
+++ src/request.ts
@@ -1,5 +1,19 @@
 import config from './config';
 
+interface Params {
+  api_key: string;
+  from?: string; // Beginning timestamp of a range - only display scrobbles after this time, in UNIX timestamp format (integer number of seconds since 00:00:00, January 1st 1970 UTC). This must be in the UTC time zone.
+  limit?: string; // The number of results to fetch per page. Defaults to 50. Maximum is 200.
+  page?: string; // The page number to fetch. Defaults to first page.
+  recenttracks?: string;
+  to?: string;
+  user: string; // The last.fm username to fetch the recent tracks of
+  tag?: string;
+  taggingtype: string;
+  extended: string;
+  period?: string; // overall | 7day | 1month | 3month | 6month | 12month
+}
+
 const request = async <Parameters, Response>(
   method: string,
   user: string,
@@ -13,7 +27,9 @@ const request = async <Parameters, Response>(
     config.api_key
   }&format=${config.format.json}`;
 
-  return (await fetch(url, {
+  return (await fetch(config.base_url, {
+    method: 'GET',
+
     headers: {
       'Content-Type': 'application/json',
     },
