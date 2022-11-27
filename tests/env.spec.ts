c2fce4c0968584815e3959a673798969da50d456
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2022-11-27 08:49:53 +0100

    setting up tests

--- tests/env.spec.ts
+++ tests/env.spec.ts
@@ -3,4 +3,9 @@ import fetch, { Request } from 'cross-fetch';
 
 const ENV_EXAMPLE = fetch('./.env.example');
 
-console.log(ENV_EXAMPLE);
+console.log('🧪 ENV');
+
+test.describe('Environment variables present and confiured', () => {
+  test('.env file is present', async () => {});
+  test('.env file has required variables', async () => {});
+});
