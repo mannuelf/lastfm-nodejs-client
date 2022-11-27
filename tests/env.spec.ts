658abbd0e2a6f36745fdb15c4d2fe0479e6d56fb
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2022-11-27 08:40:57 +0100

    clean up test config

--- tests/env.spec.ts
+++ tests/env.spec.ts
@@ -1,1 +1,6 @@
+import { test, expect, type Page } from '@playwright/test';
+import fetch, { Request } from 'cross-fetch';
 
+const ENV_EXAMPLE = fetch('./.env.example');
+
+console.log(ENV_EXAMPLE);
