118955aff6d7fb62255f7a5ee0dacd3c4696c60c
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2023-09-12 07:05:08 +0200

    typing: adds typing

--- src/method.ts
+++ src/method.ts
@@ -1,3 +1,21 @@
+interface UserMethod {
+  getInfo: string;
+  loved_tracks: string;
+  recent_tracks: string;
+  top_albums: string;
+  top_artists: string;
+  top_tracks: string;
+  weekly_album_chart: string;
+  weekly_artist_chart: string;
+  weekly_chart_list: string;
+  weekly_track_chart: string;
+};
+
+export interface Method {
+  auth: string;
+  user: UserMethod;
+};
+
 export default {
   auth: 'auth.getToken',
   user: {
@@ -12,4 +30,4 @@ export default {
     weekly_chart_list: 'user.getWeeklyChartList',
     weekly_track_chart: 'user.getWeeklyTrackChart',
   },
-};
+} satisfies Method;
