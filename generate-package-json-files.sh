1b080b001bdf38f7efe5a85515fe27a74a9602ad
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2023-10-02 22:35:03 +0200

    script to help move files along

---
+++ generate-package-json-files.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Add package.json files to cjs/mjs folders.
+
+cat > dist/cjs/package.json <<!EOF
+{
+    "type": "commonjs"
+}
+!EOF
+
+cat > dist/mjs/package.json <<!EOF
+{
+    "type": "module"
+}
+!EOF
+
+find src -name '*.d.ts' -exec cp {} dist/mjs \;
+find src -name '*.d.ts' -exec cp {} dist/cjs \;
