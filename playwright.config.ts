7d8d0d586ffa904286430b3b951e57b1fc8c9525
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2026-03-17 06:29:49 +0100

    feat: update node, remove dotenv

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,6 +1,4 @@
 import { devices, PlaywrightTestConfig } from '@playwright/test';
-import * as dotenv from 'dotenv'; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
-dotenv.config();
 
 /**
  * See https://playwright.dev/docs/test-configuration.
--- src/request.ts
+++ src/request.ts
@@ -1,4 +1,3 @@
-import fetch from 'cross-fetch';
 import { config } from './config.js';
 
 interface RequestOptions {
